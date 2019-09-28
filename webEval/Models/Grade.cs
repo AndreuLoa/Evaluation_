f2398d4b189c7f846932922d8094d0d5031d58d2
Andreu Loayza <andreu.loayza@hotmail.com>
2019-09-27 20:15:56 -0400

    commit inicial

---
+++ webEval/Models/Grade.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace webEval.Models
+{
+    public class Grade
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public int score { get; set; }
+        [Required]
+        public string subject { get; set; }
+    }
+}
